37eda766f4621e227740a0731b60ddec6ad3861b
Charlie Brown <carbonrobot@gmail.com>
2015-02-13 14:51:19 -0600

    Refactor NOAA http calls to a separate service

--- server/controllers/stations.controller.js
+++ server/controllers/stations.controller.js
@@ -2,15 +2,9 @@
 
 var Hapi = require('hapi'),
     config = require('../../config/config.js'),
-    httpClient = require('request-json'),
-    httpRequest = require('request'),
-    xml = require('xml2js'),
-    moment = require('moment'),
-    _ = require('lodash');
+    noaa = require('../services/noaa.service.js');
 
 function StationsController() {
-
-    var noaaBaseUri = 'http://www.ndbc.noaa.gov/';
     
     /**
      * @name getStation
@@ -22,54 +16,10 @@ function StationsController() {
             return reply('Missing station Id. /stations/{stationId}').code(401);
         }
 
-        var uri = noaaBaseUri + 'get_observation_as_xml.php?station=' + id;
-        httpRequest(uri, function (err, res, body) {
-            if (err) {
-                console.log(err);
-                return reply('No station found for id ' + id).code(404);
-            }
-
-            xml.parseString(body, { trim: true }, function (xmlErr, result) {
-                if (xmlErr) {
-                    console.log(xmlErr);
-                    return reply('Unable to parse station data for id ' + id).code(500);
-                }
-
-                result = result.observation;
-                var json = {
-                    id: result.$.id,
-                    name: result.$.name,
-                    lat: result.$.lat,
-                    lon: result.$.lon,
-                    data: []
-                };
-                if (result.datetime) {
-                    json.datetime = moment(result.datetime[0].replace(/UTC/, 'Z'));
-                }
-
-                parse(json, result, [
-                    [ 'Air Temp', 'airtemp' ],
-                    [ 'Water Temp', 'watertemp'],
-                    [ 'Pressure', 'pressure'],
-                    [ 'Wind Direction', 'winddir'],
-                    [ 'Wind Speed', 'windspeed'],
-                    [ 'Wind Gust', 'windgust']
-                ]);
-                
-                return reply(JSON.stringify(json));
-            });
-        });
-    }
-
-    function parse(r, d, a) {
-        a.forEach(function(s) {
-            if (d[s[1]]) {
-                r.data.push({
-                    name: s[0],
-                    value: d[s[1]][0]._,
-                    unit: d[s[1]][0].$.uom
-                });
-            }
+        noaa.getObservations(id).then(function(data) {
+            return reply(data);
+        }, function() {
+            return reply('No station found for id ' + id).code(404);
         });
     }
 
@@ -78,15 +28,10 @@ function StationsController() {
      * @desc Gets a list of known NOAA buoy stations
      */
     function list(request, reply) {
-        var client = httpClient.newClient(noaaBaseUri);
-        var uri = 'ndbcmapstations.json';
-
-        client.get(uri, function (err, res, body) {
-            if (err) {
-                return console.log(err);
-            }
-
-            return reply(body.station);
+        noaa.getStations().then(function(data) {
+            return reply(data);
+        }, function(err) {
+            return reply('Could not obtain a list of stations from NOAA\r\n' + err).code(500);
         });
     }
 
---
+++ server/services/noaa.service.js
@@ -0,0 +1,104 @@
+﻿'use strict';
+
+var httpClient = require('request-json'),
+    httpRequest = require('request'),
+    moment = require('moment'),
+    xml = require('xml2js'),
+    q = require('q');
+
+function NoaaServices() {
+
+    var noaaBaseUri = 'http://www.ndbc.noaa.gov/';
+
+    /**
+     * @name getObservations
+     * @desc Get current observations for the given station id
+     */
+    function getObservations(stationId) {
+        var deferred = q.defer();
+
+        var uri = noaaBaseUri + 'get_observation_as_xml.php?station=' + stationId;
+        httpRequest(uri, function(err, res, body) {
+            if (err) {
+                console.log(err);
+                deferred.reject(err);
+            } else {
+
+                xml.parseString(body, {trim: true}, function(xmlErr, result) {
+                    if (xmlErr) {
+                        console.log(xmlErr);
+                        console.log('Unable to parse station data for id ' + stationId);
+                        deferred.reject(err);
+                    } else {
+
+                        result = result.observation;
+                        var json = {
+                            id: result.$.id,
+                            name: result.$.name,
+                            lat: result.$.lat,
+                            lon: result.$.lon,
+                            data: []
+                        };
+                        if (result.datetime) {
+                            json.datetime = moment(result.datetime[0].replace(/UTC/, 'Z'));
+                        }
+
+                        parse(json, result, [
+                            ['Air Temp', 'airtemp'],
+                            ['Water Temp', 'watertemp'],
+                            ['Pressure', 'pressure'],
+                            ['Wind Direction', 'winddir'],
+                            ['Wind Speed', 'windspeed'],
+                            ['Wind Gust', 'windgust']
+                        ]);
+
+                        deferred.resolve(JSON.stringify(json));
+                    }
+                });
+
+            }
+        });
+
+        return deferred.promise;
+    }
+
+    /**
+     * @name getStations
+     * @desc Get a list of all known NOAA stations
+     */
+    function getStations() {
+        var deferred = q.defer();
+        var client = httpClient.newClient(noaaBaseUri);
+
+        client.get('ndbcmapstations.json', function(err, res, body) {
+            if (err) {
+                console.log(err);
+                deferred.reject(err);
+            } else {
+                deferred.resolve(body.station);
+            }
+        });
+
+        return deferred.promise;
+    }
+
+    // internal parser for noaa data
+    function parse(r, d, a) {
+        a.forEach(function (s) {
+            if (d[s[1]]) {
+                r.data.push({
+                    name: s[0],
+                    value: d[s[1]][0]._,
+                    unit: d[s[1]][0].$.uom
+                });
+            }
+        });
+    }
+
+    return {
+        getObservations: getObservations,
+        getStations: getStations
+    };
+}
+
+module.exports = NoaaServices();
